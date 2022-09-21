3374df6b326e962f207d5044a8356ae67090f879
Austin Barrington <arbarrington12@gmail.com>
2022-09-21 09:50:07 -0600

    commit tree 033a6b042ee8720d9a8291a1091b3783ae46ac73

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
+    # attr_reader :name
+    # attr_writer :""
+    # attr_accessor :""
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    # attr_reader :name
+    # attr_writer :""
+    # attr_accessor :""
 
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name 
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job 
+    end
 end
